3c5cfff5a3450469834f1af13ec736fba9065324
Matthew Hallquist <matthallquist@yahoo.com>
2016-12-18 15:18:40 +0000

    added @@all variable, reader, and save method for beer and brewery classes

--- lib/beer.rb
+++ lib/beer.rb
@@ -1,12 +1,23 @@
 class Beer
   attr_accessor :rating, :name, :brewery, :type, :abv
 
+  @@all = []
+
   def initialize(rating, name, brewery, type, abv)
     @rating = rating
     @name = name
     @brewery = brewery
     @type = type
     @abv = abv
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
   end
 
 end
--- lib/brewery.rb
+++ lib/brewery.rb
@@ -1,9 +1,20 @@
 class Brewery
-attr_accessor :name
+  attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
     @beers = []
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
   end
 
 end
